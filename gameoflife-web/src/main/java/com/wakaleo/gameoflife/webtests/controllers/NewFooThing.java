e2c09178b6f75403ff9fe451d58eaa40b5659598
Jim Nicholson <james.nicholson@addteq.com>
2016-06-10 17:00:57 -0400

    SD-4 Add a new foo thing.

---
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/NewFooThing.java
@@ -0,0 +1,10 @@
+package com.wakaleo.gameoflife.webtests.controllers;
+
+public class NewFooThing {
+
+    public int compareTo(NewFooThing that) {
+	// MUCH BETTER THAN THE OLD FOO THING. 
+	return Integer.MIN_VALUE;
+    }
+
+}
